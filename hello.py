b959451bbb21a4c6174a482a9f96da1e5552f7d0
Maya Warrier <maya.warrierm@gmail.com>
2023-10-16 16:30:01 -0400

    Activity 4: Add email address field with UofT validation - Validates that email comes from "utoronto" domain - If not, tells user to use UofT email

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
@@ -14,6 +14,7 @@ moment = Moment(app)
 
 class MainForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -23,13 +24,27 @@ def index():
 
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
+
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
 
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
 
-    return render_template('index.html', form=form, name=session.get('name'))
+    email_msg = None
+    email = session.get('email')
+    if email is not None:
+        if "utoronto" in email:
+            email_msg = 'Your UofT email is ' + email
+        else:
+            email_msg = 'Please use your UofT email.'
+
+    return render_template('index.html', form=form,
+        name=session.get('name'), email_msg=email_msg)
 
 
 @app.route('/user/<name>')
