2b99db57516a63c7ccca601199e0d50bb3fbd9ad
Maya Warrier <maya.warrierm@gmail.com>
2023-10-16 15:18:29 -0400

    Do example 4-7 from book: Web forms and message flashing - Use Bootstrap and flask_wtf to make fancy validated form - Flash message when name changes

--- hello.py
+++ hello.py
@@ -1,21 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hBGQNMa7wlpWGxN8z5B0m0qNNQz8WfQe' # flask wtf
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+
+class MainForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = MainForm()
+
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form=form, name=session.get('name'))
+
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
-# Not sure if this is required? I did it because it's part of Chapter 3
+
+# Not sure if this is required? it's part of Chapter 3
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
